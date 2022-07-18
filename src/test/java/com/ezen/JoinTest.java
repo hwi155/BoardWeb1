39f854966b7a17054af4915220a84dded25a4b7e
youngtae <pyt3692@naver.com>
2022-07-18 13:06:13 +0900

    이슈 내용 수정

--- src/test/java/com/ezen/JoinTest.java
+++ src/test/java/com/ezen/JoinTest.java
@@ -12,6 +12,6 @@ public class JoinTest {
 	@Test
 	public void JoinTest() {
 		System.out.println("feature/1 브랜치 테스트 코드");
-		System.out.println("issue#1에 구현기기로 한 회원가입을 구현한다.");
+		System.out.println("issue#1 회원가입 구현완료.");
 	}
 }
