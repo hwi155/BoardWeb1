41cd833116dea888c5e890c4f750c96690fddd0e
youngtae <pyt3692@naver.com>
2022-07-18 12:40:45 +0900

    [Add] 회원가입 기능 구현 완료

---
+++ src/test/java/com/ezen/JoinTest.java
@@ -0,0 +1,17 @@
+package com.ezen;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoinTest {
+
+	@Test
+	public void JoinTest() {
+		System.out.println("feature/1 브랜치 테스트 코드");
+		System.out.println("issue#1에 구현기기로 한 회원가입을 구현한다.");
+	}
+}
